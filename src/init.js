4b79ae851bf46e50ff371fb94f80d0cc9ce8b812
hbjORbj <sldisek783@gmail.com>
2020-04-09 14:21:21 +0100

    commit

--- src/init.js
+++ src/init.js
@@ -14,7 +14,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
